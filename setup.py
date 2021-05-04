7a225894d28881c81e805ff8052bb6a49b815a00
Nathan Sain <nathan.sain@gmail.com>
2021-05-04 14:28:01 -0500

    include static assets

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='foundryWorldTools',
     version='0.2',
     packages=['foundryWorldTools'],
+    package_data = {
+        'foundryWorldTools': ['*.json']
+    },
     include_package_data=True,
     install_requires=[
         'Click',
