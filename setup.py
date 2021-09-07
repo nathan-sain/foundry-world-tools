dd9291b9d9d316ec43d3755af0196c647cd29fbc
Nathan Sain <nathan.sain@gmail.com>
2021-09-07 02:36:36 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='foundryWorldTools',
-    version='0.4.4',
+    version='0.4.5',
     packages=['foundryWorldTools'],
     package_data = {
         'foundryWorldTools': ['*.json']
